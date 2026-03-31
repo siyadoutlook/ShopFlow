6cbd91fb06978f8609eab2c387a2ae072da5e6b6
siyad k k <siyad@outlook.com>
2026-03-31 17:59:31 +0100

    GRPC services added for inventory and order service. Inventory service checks for available stock

--- ShopFlow.InventoryService/Program.cs
+++ ShopFlow.InventoryService/Program.cs
@@ -1,10 +1,14 @@
+using ShopFlow.InventoryService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 app.MapControllers();
+app.MapGrpcService<InventoryGrpcService>();
 
 app.Run();
 
---
+++ ShopFlow.OrderService/Controllers/OrderController.cs
@@ -0,0 +1,53 @@
+﻿using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using ShopFlow.OrderService.Data;
+using ShopFlow.OrderService.Models;
+using ShopFlow.Shared.Protos;
+
+namespace ShopFlow.OrderService.Controllers;
+
+[Route("orders")]
+[ApiController]
+public class OrderController(InventoryService.InventoryServiceClient inventoryClient) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<Order>> GetAll()
+    {
+        return Ok(OrderStore.Orders);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Order>> Create(Order order)
+    {
+        var stockResponse = await inventoryClient.CheckStockAsync(new StockRequest()
+        {
+            ProductId = order.ProductId,
+            Quantity = order.Quantity
+        });
+
+        if (!stockResponse.IsAvailable)
+        {
+            return BadRequest(new
+            {
+                message = $"Insufficient stock. Available: {stockResponse.AvailableStock}"
+            });
+        }
+
+        order.Id = OrderStore.Orders.Count + 1;
+        order.OrderStatus = OrderStatus.Pending;
+        OrderStore.Orders.Add(order);
+
+        return CreatedAtAction(nameof(GetById), new { id = order.Id }, order);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Order> GetById(int id)
+    {
+        var order = OrderStore.Orders.FirstOrDefault(o => o.Id == id);
+
+        if (order is null)
+            return NotFound();
+
+        return Ok(order);
+    }
+}
